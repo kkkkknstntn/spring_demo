9c6b7e49662cfa40c1978157f33cbf59fd555565
Кириленко Константин <p4vukkk@yandex.ru>
2024-10-10 03:06:49 +0400

    docs

--- src/main/java/com/example/demo/service/UserServiceImpl.java
+++ src/main/java/com/example/demo/service/UserServiceImpl.java
@@ -32,24 +32,30 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public Mono<UserResponseDTO> create(UserRequestDTO userDTO) {
-        return userRepository.save(
-                build(userDTO, Provider.PASSWORD )
+        User user = build(userDTO);
+        return userRepository.save(user.toBuilder()
+                .provider(Provider.PASSWORD)
+                .password(passwordEncoder.encode(user.getPassword()))
+                .vkId(null)
+                .build()
         ).doOnSuccess(u -> log.info("IN create - user: {} created", u)).map(userMapper::responseMap);
     }
 
     public Mono<UserResponseDTO> createVk(UserRequestDTO userDTO) {
-        return userRepository.save(
-                build(userDTO, Provider.VK )
+        User user = build(userDTO);
+        return userRepository.save(user.toBuilder()
+                .provider(Provider.PASSWORD)
+                .password(passwordEncoder.encode(user.getPassword()))
+                .vkId(null)
+                .build()
         ).doOnSuccess(u -> log.info("IN createVk - user: {} created", u)).map(userMapper::responseMap);
     }
 
-    private User build (UserRequestDTO userDTO, Provider provider){
+    private User build (UserRequestDTO userDTO){
         User user = userMapper.requestMap(userDTO);
         return  user.toBuilder()
-                .password(provider == Provider.PASSWORD ? passwordEncoder.encode(user.getPassword()) : null)
                 .role(UserRole.USER)
                 .enabled(true)
-                .provider(provider)
                 .createdAt(LocalDateTime.now())
                 .updatedAt(LocalDateTime.now())
                 .build();
